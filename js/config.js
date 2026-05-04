8ba1908b7b65966dba6d6ff24d51c9e65d79b466
Deepak Pokhrel <your_email_for_DeepakPokhrel2030-star@example.com>
2026-05-04 14:59:12 +0100

    Add Azure Blob Storage uploads with SAS token

--- js/config.js
+++ js/config.js
@@ -1,9 +1,13 @@
-const CONFIG = {
-  ENDPOINTS: {
-    getAssets: 'https://prod-31.italynorth.logic.azure.com:443/workflows/c64ebce138ef4f33bf34ce26c8b574bb/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=ImWt9FgUtOncKgI7XUq1QFcCp9GWrbYHaot3tKgec5c',
-    getAsset: 'https://prod-02.italynorth.logic.azure.com:443/workflows/456c8b63669948d2bfa3add775bcb1a1/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=Ndi_WwbCi9qQU7G8S6mlo0nvbeE0HKtIC7x-9d5UB6s',
-    createAsset: 'https://prod-11.italynorth.logic.azure.com:443/workflows/e377fa9862474c609f7b38343fa818e4/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=r3uN25vZQR-FUpWQPDMPRNdkT7TNbRZejzWwPJvzvls',
-    updateAsset: 'https://prod-05.italynorth.logic.azure.com:443/workflows/ff77e69c87204177880b1c48ba5e24d1/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=bSGN8tS7zecU3Wv8Brrg073sts5buPnlWIIa6PAzups',
-    deleteAsset: 'https://prod-05.italynorth.logic.azure.com:443/workflows/aaf55a4039504aaaba5c38f2c5f237be/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=RjrBLdl0ZOsC3HYKFKS3jrv9t3OxIjnfT9gMLsQ9kLw'
+const CONFIG={
+  ENDPOINTS:{
+    getAssets:'https://prod-31.italynorth.logic.azure.com:443/workflows/c64ebce138ef4f33bf34ce26c8b574bb/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=ImWt9FgUtOncKgI7XUq1QFcCp9GWrbYHaot3tKgec5c',
+    getAsset:'https://prod-02.italynorth.logic.azure.com:443/workflows/456c8b63669948d2bfa3add775bcb1a1/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=Ndi_WwbCi9qQU7G8S6mlo0nvbeE0HKtIC7x-9d5UB6s',
+    createAsset:'https://prod-11.italynorth.logic.azure.com:443/workflows/e377fa9862474c609f7b38343fa818e4/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=r3uN25vZQR-FUpWQPDMPRNdkT7TNbRZejzWwPJvzvls',
+    updateAsset:'https://prod-05.italynorth.logic.azure.com:443/workflows/ff77e69c87204177880b1c48ba5e24d1/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=bSGN8tS7zecU3Wv8Brrg073sts5buPnlWIIa6PAzups',
+    deleteAsset:'https://prod-05.italynorth.logic.azure.com:443/workflows/aaf55a4039504aaaba5c38f2c5f237be/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=RjrBLdl0ZOsC3HYKFKS3jrv9t3OxIjnfT9gMLsQ9kLw'
+  },
+  BLOB:{
+    baseUrl:'https://stheritaguard916368.blob.core.windows.net/media',
+    sasToken:'?sv=2025-11-05&ss=bfqt&srt=o&sp=rwdlacupiytfx&se=2027-05-04T21:56:26Z&st=2026-05-04T13:41:26Z&spr=https&sig=aX0v7%2F8lcLM9QAJJBgq%2B%2Bs27qVNFA9aCq50H3CEp5Gk%3D'
   }
 }
